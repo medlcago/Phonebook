b24bae63d5578ce435bc734fcb732e95e13c0621
medlcago <sasha.vol40@mail.ru>
2023-12-29 01:06:42 +0300

    Added getting entries

--- api/src/routers/phonebook/crud.py
+++ api/src/routers/phonebook/crud.py
@@ -1,18 +1,34 @@
+from typing import Sequence
+
 from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models import PhoneBook
 
 
-async def create_entry(db: AsyncSession, data: dict):
+async def create_entry(db: AsyncSession, data: dict) -> PhoneBook:
     try:
         entry = PhoneBook(**data)
         entry.phone_number = "+7" + entry.phone_number[1:]
-        print(entry.phone_number)
         db.add(entry)
         await db.commit()
         await db.refresh(entry)
         return entry
     except IntegrityError:
         raise HTTPException(status_code=409, detail=f"The phone number {entry.phone_number} is already recorded.")
+
+
+async def get_entry_by_id(db: AsyncSession, entry_id: int, user_id: str) -> PhoneBook:
+    entry = await db.scalar(select(PhoneBook).filter_by(user_id=user_id).filter_by(id=entry_id))
+    if entry is None:
+        raise HTTPException(status_code=404, detail=f"Entry with id {entry_id} not found.")
+    return entry
+
+
+async def get_all_entries_by_user_id(db: AsyncSession, user_id: str) -> Sequence[PhoneBook]:
+    entries = (await db.scalars(select(PhoneBook).filter_by(user_id=user_id))).all()
+    if entries:
+        return entries
+    raise HTTPException(status_code=404, detail="No entries found.")
--- api/src/routers/phonebook/router.py
+++ api/src/routers/phonebook/router.py
@@ -11,7 +11,25 @@ router = APIRouter(prefix="/phonebook", tags=["Phonebook"])
 
 
 @router.post("/", summary="Создание новой записи", response_model=PhoneBookEntryRead)
-async def create_entry(entry: PhoneBookEntryCreate, user: ResponseUser = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+async def create_entry(entry: PhoneBookEntryCreate,
+                       user: ResponseUser = Depends(get_current_user),
+                       db: AsyncSession = Depends(get_db)
+                       ):
     data = entry.model_dump()
     data.update(user_id=user.user_id)
     return await crud.create_entry(db=db, data=data)
+
+
+@router.get("/id{entry_id}", summary="Получить запись по ID", response_model=PhoneBookEntryRead)
+async def get_entry_by_id(entry_id: int,
+                          user: ResponseUser = Depends(get_current_user),
+                          db: AsyncSession = Depends(get_db)
+                          ):
+    return await crud.get_entry_by_id(db=db, entry_id=entry_id, user_id=str(user.user_id))
+
+
+@router.get("/user/entries", summary="Все записи пользователя", response_model=list[PhoneBookEntryRead])
+async def get_all_entries_by_user_id(user: ResponseUser = Depends(get_current_user),
+                                     db: AsyncSession = Depends(get_db)
+                                     ):
+    return await crud.get_all_entries_by_user_id(db=db, user_id=str(user.user_id))
